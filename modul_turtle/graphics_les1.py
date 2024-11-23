3a9110d835f52fa0652293448ec1c7f40c990853
Elena-kontaurova <kontaurova0306@gmail.com>
2024-11-23 09:17:12 +0300

    конец первого

--- modul_turtle/graphics_les1.py
+++ modul_turtle/graphics_les1.py
@@ -1,8 +1,25 @@
 ''' Модуль Turtle'''
 import turtle
+import random
 
 t = turtle.Turtle()
 
-for _ in range(4):
-    t.forward(100)
-    t.right(90)
+
+def square(pixels):
+    ''' Рисуем квадрат'''
+    for _ in range(4):
+        t.forward(pixels)
+        t.right(90)
+
+
+t.speed(25)
+
+list_colors = ['red', 'green', 'yellow', 'orange', 'pink']
+
+
+initial_step = 30
+for _ in range(60):
+    t.color(random.choice(list_colors))
+    t.right(10)
+    square(initial_step)
+    initial_step += 4
